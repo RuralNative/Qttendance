23b2d4c6d7b257b5dea35235a9f657af48b1c043
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-05-14 16:15:49 +0800

    Updated Start Script
    Lengthen artificial delay, added note for additional text on window, and optimized window closing function

--- Qttendance_Start.ps1
+++ Qttendance_Start.ps1
@@ -1,22 +1,36 @@
 # Print Introductory Text
 Write-Host "Qttendance: Attendance Monitoring System with QR Code Scanner"
 Write-Host "An free and open-source solution for monitoring attendance for organizations"
-Write-Host "____________________________________________________________________________"
+Write-Host "----------------------------------------------------------------------------"
 Write-Host "Github: RuralNative | Developing Solutions One Line at a Time"
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 # Start XAMPP Control Panel
 Start-Process "C:\xampp\xampp-control.exe"
 Write-Host "DO NOT CLOSE | XAMPP Control Starting"
-Start-Sleep -Seconds 2
+Write-Host "During startup, several text scripts will show on-screen."
+Write-Host "----------------------------------------------------------------------------"
+Start-Sleep -Seconds 5
 
 # Start necessary modules (Apache and MySQL)
 Start-Process "C:\xampp\apache_start.bat" -NoNewWindow
 Start-Process "C:\xampp\mysql_start.bat" -NoNewWindow
 
 # Wait for Apache and MySQL to start
-Start-Sleep -Seconds 3
-exit
+Write-Host "----------------------------------------------------------------------------"
+Start-Sleep -Seconds 5
 
 # Open a new browser window and navigate to localhost/qttendance
 Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost/qttendance"
+Write-Host "Qttendance Successfully Opened."
+Write-Host "Current window will close after 5 seconds. If none happens, you may now SAFELY CLOSE this window"
+
+# Attempt to close a PowerShell window
+Start-Sleep -Seconds 5
+$process = Get-Process -Name powershell
+if ($process) {
+    $process.CloseMainWindow()
+    Write-Host "Attempted to close PowerShell window"
+} else {
+    Write-Host "No PowerShell windows found."
+}
