3b48dafe2459b4617fa87fab060b1d6e7499b58b
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-06-10 18:21:46 +0800

    Fix for Admin UI Errors
    Modified resource paths for the CSS/JS inclusion for Admin component, and removed duplicated Admin page

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="../global/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="../global/dist/css/skins/_all-skins.min.css">
   	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   	<!--[if lt IE 9]>
